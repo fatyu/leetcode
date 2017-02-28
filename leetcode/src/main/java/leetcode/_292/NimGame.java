40b68c8df8253205c0d4629b54aedec896d01283
fatyu <33862677@qq.com>
2017-02-28 14:17:20 +0800

    finish nim game

--- leetcode/src/main/java/leetcode/_292/NimGame.java
+++ leetcode/src/main/java/leetcode/_292/NimGame.java
@@ -5,30 +5,31 @@ package leetcode._292;
  *  The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
  *	Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
  *	For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+ *
+ *  https://en.wikipedia.org/wiki/Nim
  */
 public class NimGame {
 
-	private int stoneNum;
+	//解决问题的关键 找寻必胜规律
+	// heap stoneNum == 1  first gamer win
+	// heap stoneNum == 2  first gamer win
+	// heap stoneNum == 3  first gamer win
+	// heap stoneNum == 4  second gamer win
+	// heap stoneNum == 5  first gamer win
+	// heap stoneNum == 6  first gamer win
+	// heap stoneNum == 7  first gamer win
+	// heap stoneNum == 8  second gamer win
+	// ...
+	// ...
 
-	public int getStoneNum() {
-		return stoneNum;
-	}
-
-	public void setStoneNum(int stoneNum) {
-		this.stoneNum = stoneNum;
-	}
-
-	private NimGame() {
-		super();
-	}
-
-	private NimGame(int stoneNum) {
-		super();
-		this.stoneNum = stoneNum;
+	public boolean canWinNim(int n) {
+		return n % 4 > 0;
 	}
 
 	public static void main(String[] args) {
-		int a = 3;
-		System.out.println(a);
+		NimGame nimGame = new NimGame();
+		for (int i = 0; i < 100; i++) {
+			System.out.println("heap stone number is " + i + ",can you win? ->" + nimGame.canWinNim(i));
+		}
 	}
 }
