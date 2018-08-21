2358b0f5cfbd1f0784bcf1f35b34cc0620a0987c
fatyu <33862677@qq.com>
2018-08-21 12:08:13 +0800

    :beers: 100:Same Tree

--- src/main/java/leetcode/algorithm/_226/TreeNode.java
+++ src/main/java/leetcode/algorithm/_226/TreeNode.java
@@ -1,10 +1,11 @@
 package leetcode.algorithm._226;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val){
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
         this.val = val;
     }
 }
