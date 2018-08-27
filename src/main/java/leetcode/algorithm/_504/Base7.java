5a3e4fd599d376392ac07550b74f2bf50a7f3e55
fatyu <33862677@qq.com>
2018-08-27 23:52:43 +0800

    :beers: 504:Base-7

---
+++ src/main/java/leetcode/algorithm/_504/Base7.java
@@ -0,0 +1,43 @@
+package leetcode.algorithm._504;
+
+/**
+ *
+ Given an integer, return its base 7 string representation.
+ Example 1:
+ Input: 100
+ Output: "202"
+ Example 2:
+ Input: -7
+ Output: "-10"
+ Note: The input will be in range of [-1e7, 1e7].
+ 递归对7求余数和求除数
+ */
+public class Base7 {
+    public String base7IntegerToString(int num) {
+        String flag = num >= 0 ? "" : "-";//判断正负数
+        int result = 0;
+        num = Math.abs(num);
+        int count = 0;
+        result = calNum(result, num, count);
+        return flag + result;
+    }
+
+    private int calNum(int result, int number, int count) {
+        count = count + 1;
+        int divide = number / 7;
+        int cal = number % 7;
+        result += (int) (Math.pow(10, count - 1) * cal);
+        if (divide > 7) {
+            result += calNum(result, divide, count);
+        } else {
+            result += (int) (Math.pow(10, count) * divide);
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Base7 base = new Base7();
+        System.out.println(base.base7IntegerToString(121));
+    }
+}
