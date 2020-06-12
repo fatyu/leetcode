e3ebd49cb5df2d11c6bf91a234d161d0cdb0a423
fatyu <33862677@qq.com>
2020-06-12 15:00:58 +0800

    :ok_hand: finished question no.28

--- src/main/java/leetcode/algorithm/math/_028/ImplementsStrStr.java
+++ src/main/java/leetcode/algorithm/math/_028/ImplementsStrStr.java
@@ -1,15 +1,88 @@
 package leetcode.algorithm.math._028;
 
+/**
+ * Implement strStr().
+ * <p>
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * Clarification:
+ * <p>
+ * What should we return when needle is an empty string? This is a great question to ask during an interview.
+ * <p>
+ * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ */
 public class ImplementsStrStr {
 
-    public int strStr(String haystack, String needle) {
+    @Deprecated
+    public int strStrFailed(String haystack, String needle) {
+        if(haystack==null){
+            return -1;
+        }
         if (null == needle || "".equals(needle)) {
             return 0;
         }
-        //暴力迭代
-        int sourceLength = haystack.length();
-        int indexLength = needle.length();
-//TODO
-        return 0;
+        if(haystack.length()<needle.length()){
+            return -1;
+        }
+        int result = -1;
+        String compartor = needle;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == compartor.charAt(0)) {
+                if (haystack.charAt(i) == needle.charAt(0) && (needle.length()-compartor.length())==0) {
+                    result = i;
+                }
+                compartor = compartor.substring(1, compartor.length());
+                if (compartor.equals("")) {
+                    return result;
+                }
+
+            } else if(haystack.charAt(i) ==  needle.charAt(0)) {
+                result = i;
+                compartor = needle;
+                compartor = compartor.substring(1, compartor.length());
+
+            }else{
+                compartor = needle;
+                result = -1;
+            }
+        }
+        return result;
+    }
+
+
+    public int strStr(String haystack, String needle) {
+        if(haystack==null){
+            return -1;
+        }
+        if (null == needle || "".equals(needle)) {
+            return 0;
+        }
+        if(haystack.length()<needle.length()){
+            return -1;
+        }
+
+
+        //使用java String 的substring方法
+        for(int i = 0;i<=(haystack.length()-needle.length());i++){
+            if(haystack.substring(i,i+needle.length()).equals(needle)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String [] args){
+        ImplementsStrStr i = new ImplementsStrStr();
+        System.out.println(i.strStr("mississippi","issip"));
     }
 }
